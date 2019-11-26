a01ed9518414e97f8d030b1bf9cec90d72ec4335
ivalderrama <ivalderrama@gmail.com>
2019-11-26 20:37:51 +0000

    Submission v3 - Because I'm a fucking retard

--- project_01_data_modeling_with_postgres/sql_queries.py
+++ project_01_data_modeling_with_postgres/sql_queries.py
@@ -72,7 +72,7 @@ user_table_insert = ("""
     INSERT INTO users 
     (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING;
+    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""
